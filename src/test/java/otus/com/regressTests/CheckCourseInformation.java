60020d09203a40be8c315d4b8aa4098e274ac294
iluha5596 <pantik96@mail.ru>
2021-11-23 14:59:18 +0300

    fix сheck SpecificationGameQAEngineer

--- src/test/java/otus/com/regressTests/CheckCourseInformation.java
+++ src/test/java/otus/com/regressTests/CheckCourseInformation.java
@@ -2,12 +2,14 @@ package otus.com.regressTests;
 
 import config.ExpectedValues;
 import org.aeonbits.owner.ConfigFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import otus.com.BaseTest;
 import otus.com.dto.ActualValues;
 import otus.com.pages.*;
 
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class CheckCourseInformation extends BaseTest {
 
@@ -41,6 +43,8 @@ public class CheckCourseInformation extends BaseTest {
         assertEquals(expectedValues.nameCourseGameQAEngineer(), actualValuesGameQAEngineer.getNameCourseGameQAEngineer());
         assertEquals(expectedValues.durationTrainingGameQAEngineer(), actualValuesGameQAEngineer.getDurationTrainingGameQAEngineer());
         assertEquals(expectedValues.formatCourseGameQAEngineer(), actualValuesGameQAEngineer.getFormatGameQAEngineer());
+        assertTrue((actualValuesGameQAEngineer.getSpecificationGameQAEngineer()).contains("Стань тестировщиком любых игр: мобильные, консольные, браузерные и ПК"));
+       // assertEquals(expectedValues.specificationGameQAEngineer(), actualValuesGameQAEngineer.getSpecificationGameQAEngineer());
         logger.info("Данные в карточке курса GameQAEngineer верные");
     }
 }
